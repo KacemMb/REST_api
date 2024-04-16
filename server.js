496fa280db09002fc64e9ae05cdb9edd34c30061
KacemMb <mabroukikacem10001@gmail.com>
2024-04-16 20:26:37 +0100

    add the UserModel

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+// use .env file
+dotenv.config();
+
+// Get the port from the environment, else use 3000
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect to the atlas cloud database
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((e) => {
+    console.log('Error while DB connecting');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
